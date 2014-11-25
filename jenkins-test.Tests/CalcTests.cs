d1eff67af3cdf339552ba8e3e530c68bbf7911d9
mturaga <maruthi.turaga@gmail.com>
2014-11-25 13:26:12 -0600

    test

---
+++ jenkins-test.Tests/CalcTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace jenkins_test.Tests
+{
+    //Comments
+    [TestFixture]
+    public class CalcTests
+    {
+
+        [Test]
+        public void Calc_Add_Test()
+        {
+            var actual = jenkins_test.Program.Add(1,1);
+            Assert.AreEqual(2, actual);
+        }
+    }
+}
---
+++ jenkins-test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jenkins_test
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+        }
+
+        public static int Add(int x, int y){
+            return x + y;
+        }
+    }
+}
